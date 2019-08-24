cd35b32601c2444ae884eba3a33531fb3b086643
Josh <32531221+joshuamainsmith@users.noreply.github.com>
2019-08-23 22:44:30 -0700

    Open Process Handle

---
+++ Open Process Handle/main.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#include <stdio.h>
+
+
+//int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR szCmdLine, int CmdShow)
+int wmain(int argc, wchar_t** argv)
+{
+	DWORD dwDesiredAccess = PROCESS_ALL_ACCESS;
+	BOOL bInheritHandle = false;
+	printf("Enter PID: ");
+	DWORD dwProcessId;// = 0x3B88;
+	scanf_s("%ld", &dwProcessId);
+	
+	HANDLE hResponse = OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId);
+
+	int r = GetLastError();
+
+	if (r != 0)
+	{
+		printf("Handle Not Created!\n");
+		printf("Error: %ld", GetLastError());
+		return 1;
+	}
+
+	if (hResponse <= 0)
+		printf("Handle Not Created!\n");
+	else
+		printf("Handle Created!\n");
+
+	
+
+	return 0;
+}
