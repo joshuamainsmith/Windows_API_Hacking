152e8758f84f99c0d77955758662d085d9463d53
Josh <32531221+joshuamainsmith@users.noreply.github.com>
2019-08-27 17:00:58 -0700

    Add files via upload

---
+++ Process Killer/main.c
@@ -0,0 +1,93 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+//int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR szCmdLine, int CmdShow)
+int wmain(int argc, wchar_t** argv)
+{
+	LPCSTR lpClassName;
+	//LPCSTR lpWindowName = "";
+	wchar_t wcWindowName[100];
+
+	printf("Enter window name for process to be terminated: ");
+	scanf("%[^\n]%*c", &wcWindowName);
+	printf("Window Name: %s\n", wcWindowName);
+
+	/*
+	HWND FindWindowA(
+  LPCSTR lpClassName,
+  LPCSTR lpWindowName
+);*/
+	HWND hWnd = FindWindowA(NULL, (LPCSTR)wcWindowName);
+
+	if (hWnd == NULL)
+	{
+		printf("Could not find window, error: %ld\n", GetLastError());
+		return 1;
+	}
+
+	DWORD dwProcessId;
+
+	/*
+	DWORD GetWindowThreadProcessId(
+	HWND    hWnd,
+	LPDWORD lpdwProcessId
+);*/
+	DWORD dwThreadId = GetWindowThreadProcessId(hWnd, &dwProcessId);
+
+	printf("Thread ID: %ld\n", dwThreadId);
+
+	DWORD dwDesiredAccess = PROCESS_ALL_ACCESS;
+	BOOL bInheritHandle = FALSE;
+	/*
+	HANDLE OpenProcess(
+  DWORD dwDesiredAccess,
+  BOOL  bInheritHandle,
+  DWORD dwProcessId
+);*/
+	HANDLE hProcess = OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId);
+
+	int r = GetLastError();
+
+	if (r != 0)
+	{
+		printf("Handle Not Created!\n");
+		printf("Error: %ld", GetLastError());
+		return 1;
+	}
+
+	if (hProcess <= 0)
+		printf("Handle Not Created!, Error: %ld\n", GetLastError());
+	else
+		printf("Handle Created!\n");
+
+	DWORD dwExitCode;
+	/*
+	BOOL GetExitCodeProcess(
+  HANDLE  hProcess,
+  LPDWORD lpExitCode
+);*/
+	r = GetExitCodeProcess(hProcess, &dwExitCode);
+
+	if (r == 0)
+	{
+		printf("Failed to retrieve termination status\n");
+		printf("Error: %ld", GetLastError());
+		return 1;
+	}
+
+	/*
+	BOOL TerminateProcess(
+  HANDLE hProcess,
+  UINT   uExitCode
+);*/
+	r = TerminateProcess(hProcess, dwExitCode);
+
+	if (r == 0)
+	{
+		printf("Termination Process Failed\n");
+		printf("Error: %ld", GetLastError());
+		return 1;
+	}
+
+	return 0;
+}
